c3f9b450093ea0fafed9763ca8efc8cbfc6471d4
Italo Alessandro <italo.silva@sptech.school>
2025-05-26 21:45:42 -0300

    projeto

---
+++ atividade-sd/demo/src/main/java/com/example/demo/AtividadeSdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeSdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeSdApplication.class, args);
+	}
+
+}
